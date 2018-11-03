fe073315ff82832a56c05e45655f4faf7d04cc3a
Joshua <joshuadstarkey@gmail.com>
2018-11-02 22:15:46 -0700

    remvoing what I don't need from this

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,66 +1,11 @@
-// Preset array that's turned into buttons on page load
-var loadArray = ['record', 'tape', 'cd', '8-track', 'walkman',
-'napster', 'vhs', 'blue-ray', 'spotify', 'winamp'];
-
-// createButtons loops through the loadArray to create a button with classes and
-// attributes for every array item and appending it to the page
-function createButtons() {
-	$("#generated-button-container").empty();
-
-	for (i = 0; i < loadArray.length; i++) {
-		var searchValue = loadArray[i];
-		var searchButton = $("<button>");
-		searchButton.attr("data-name", searchValue);
-		searchButton.addClass("btn btn-secondary search-button");
-		searchButton.text(searchValue);
-		$("#generated-button-container").append(searchButton);
-	}
-}
-
-// Triggering the createButtons function when page load happens
-createButtons();
-
 // on click of the submit button, create a new button with search value
-$("#search-submit").on("click", AddButton);
-
-function AddButton() {
-	event.preventDefault();
-	var search = $("#search-input").val().trim().toLowerCase();
-
-	// Prevent empty buttons from being created
-	if (search === "") {
-		alert("You have to enter a search term to create a button");
-	}
-	// Prevent duplicate buttons from being created by checking if the search
-	// term is already in the loadArray
-	else if (loadArray.includes(search)) {
-		$("#search-input").val("");
-		// animate existing button
-		shakeExistingButton(search);
-	} else {
-		loadArray.push(search);
-		$("#search-input").val("");
-		createButtons();
-	}
-}
-
-function shakeExistingButton(search) {
-	// setting animate values on the duplicate button
-	$("[data-name=" + search + "]").css("animation", "shake 0.5s");
-	$("[data-name=" + search + "]").css("animation-iteration-count", "1");
-	// waiting a 1/2 and then removing one of the properties
-	// This in incase the user trys to enter the same text again, animation is 
-	// only triggered a second time if animation css is removed so it can be reset next time
-	setTimeout(function(){ 
-		$("[data-name=" + search + "]").css("animation", "");
-	}, 500);
-}
+$("#search-submit").on("click", showMeGifs);
 
 // Takes the data-name off the button, searches the giphy API and prepends
 // 10 results to the page
 function showMeGifs() {
 	event.preventDefault();
-	var searchTerm = $(this).attr("data-name");
+	var searchTerm = $("#search-input").val().trim();
 	var giphyKey = "lG9ckorGrueBoeXQXp16qJ66xivTGyd1";
 	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchTerm +
 	 "&api_key=" + giphyKey + "&limit=10";
@@ -106,8 +51,6 @@ function gifStartStop() {
 		$(this).attr("data-state", "still");
 	}
 }
-// Listening to the document for newly generated button, 
-// on click run the showMeGifs function
-$(document).on("click", ".search-button", showMeGifs);
+
 // listeing to clicks on the returned images to trigger gifState and start/stop the gif
 $(document).on("click", ".returned-image", gifStartStop);
