e0dc7924473a63b85df7d5b9893ed8fa52e113ff
Joshua <joshuadstarkey@gmail.com>
2018-11-05 10:01:45 -0800

    cleaning up code, adding more comments.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,8 @@
-// on click of the submit button, create a new button with search value
+// On click of the submit button, trigger the showMeGifs function
 $("#search-submit").on("click", showMeGifs);
 
-// Takes the data-name off the button, searches the giphy API and prepends
-// 10 results to the page
+// Takes the search from the input, searches the giphy API and prepends
+// the first 10 results inside the search modal
 function showMeGifs() {
 	event.preventDefault();
 	var searchTerm = $("#search-input").val().trim();
@@ -20,24 +20,25 @@ function showMeGifs() {
 			var animateURL = response.data[i].images.downsized.url;
 			var stillURL = response.data[i].images.original_still.url;
 
-			// forming an element for the return item
-			var wrapper = $("<div class='col-sm-6 wrapper'>");
-			var startStopButton = $("<input type='button' class='btn btn-secondary startStopButton' value='start/stop'>");
+			// forming an element for the returned data
+			var wrapper = $("<div class='col-sm-6 text-center wrapper'>");
+			var startStopButton = $("<input type='button' class='btn btn-secondary start-stop-button' value='start/stop'>");
+			var copyButton = $("<input type='button' id='copy-button' class='btn btn-secondary copy-button' value='copy'>");
 			var image = $("<img class='returned-image'>").attr("src", animateURL);
 			image.attr("data-still", stillURL);
 			image.attr("data-animate", animateURL);
 			image.attr("data-state", "animate");
 
-			// adding the created image and start and stop buttons together to the wrapper
-			wrapper.append(image, startStopButton);
+			// adding the created image and start and stop and copy buttons together to the wrapper
+			wrapper.append(image, startStopButton, copyButton);
 			// rendering the wrapper to the #results-container
 			$("#results-container").prepend(wrapper);
 		}
 	});
 };
 
-// Listening to clicks on the Start/Stop buttons to trigger start & stop the gif playback
-$(document).on("click", ".startStopButton", gifStartStop);
+// On click of the Start/Stop buttons, trigger the gifStartStop function which pauses playback of the gif
+$(document).on("click", ".start-stop-button", gifStartStop);
 
 // Starting and Stopping the gif by swapping out the src="" URLs
 function gifStartStop() {
@@ -54,30 +55,19 @@ function gifStartStop() {
 	}
 }
 
+// Listening to clicks on the copy buttons to trigger the copy to clipboard function
+$(document).on("click", ".copy-button", copyToClipboard);
 
 
-function copyImage(url) {
-	var img = document.createElement('img');
-	img.src = url;
-	document.body.appendChild(img);
-	var r = document.createRange();
-	r.setStartBefore(img);
-	r.setEndAfter(img);
-	r.selectNode(img);
-	var sel = window.getSelection();
-	sel.addRange(r);
-	document.execCommand('Copy');
-}
-
-
-function copyGifToClipboard() {
-	// Copy gif to clipboard on click
-	var url = $(this).src;
-	console.log(url);
-	var copyGifButton = $(".returned-image");
-	console.log(copyGifButton);
-
-	// copyTextToClipboard('Gif Clicked');
-	copyImage(this.url);
-	console.log("Copy to clipboard function triggered");
+// TODO: This function is not yet working. If I had more time with this assignment, I would refactor this
+// by including clipboard.js in my project "https://clipboardjs.com/". From what I read this would make 
+// interacting with the user's clipboard much easier than attempting to to it nativly like I have attempted.
+// Once the gif is copied to the user's clipboard, they would be able to paste the selected gif in the article
+// or posting of their choice.  
+function copyToClipboard() {
+	var source = $(this).attr("src");
+	console.log(source);
+	source.src();
+	document.execCommand("copy");
+	console.log("Copy to clipboard");
 }
